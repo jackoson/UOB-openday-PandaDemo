a309919f03d93dada628a2c5d6541fa80d0db47f
Julian Loscombe <julian.loscombe@gmail.com>
2015-04-07 21:18:20 +0100

    Error was with compiling

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 cd "/Users/ben_milnes/Documents/Projects/CompSci/PANDA2/Git/panda-cwk6"
-javac -d "bin/classes" src/client/algorithms/*.java src/client/application/*.java src/client/model/*.java src/client/scotlandyard/*.java src/client/view/*.java
-cd bin/classes
+cd src
+javac -d ../bin/classes client/algorithms/*.java client/application/*.java client/model/*.java client/scotlandyard/*.java client/view/*.java
+cd ../bin/classes
 jar cfm ../jar/scotlandyardapplication.jar ../../Manifest.txt client/algorithms/*.class client/application/*.class client/model/*.class client/scotlandyard/*.class client/view/*.class resources/counters/*.png resources/players/*.png resources/tickets/*.png resources/*.png resources/*.jpg resources/*.txt
 cd ../jar
 java -jar scotlandyardapplication.jar

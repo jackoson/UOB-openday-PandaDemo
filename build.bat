a59b011cc79f3e356d698b4f2d8053d57e2c0b11
bdkmilne <ben.kelk.milne@gmail.com>
2015-04-13 16:26:50 +0100

    gitignore

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 cd "/Users/ben_milnes/Documents/Projects/CompSci/PANDA2/Git/panda-cwk6"
-cd src
-javac -d ../bin/classes client/algorithms/*.java client/application/*.java client/model/*.java client/scotlandyard/*.java client/view/*.java
-cd ../bin/classes
+javac -d "bin/classes" src/client/algorithms/*.java src/client/application/*.java src/client/model/*.java src/client/scotlandyard/*.java src/client/view/*.java
+cd bin/classes
 jar cfm ../jar/scotlandyardapplication.jar ../../Manifest.txt client/algorithms/*.class client/application/*.class client/model/*.class client/scotlandyard/*.class client/view/*.class resources/counters/*.png resources/players/*.png resources/tickets/*.png resources/*.png resources/*.jpg resources/*.txt
 cd ../jar
 java -jar scotlandyardapplication.jar

3a4be11275a7cd41220f552816fc5832eee7a92b
Julian Loscombe <julian.loscombe@gmail.com>
2015-04-23 17:51:47 +0100

    Hooking up the GUI

---
+++ src/player/GeneHunt.java
@@ -0,0 +1,56 @@
+package player;
+
+import scotlandyard.Move;
+import scotlandyard.Player;
+import scotlandyard.ScotlandYardView;
+
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * The RandomPlayer class is an example of a very simple AI that
+ * makes a random move from the given set of moves. Since the
+ * RandomPlayer implements Player, the only required method is
+ * notify(), which takes the location of the player and the
+ * list of valid moves. The return value is the desired move,
+ * which must be one from the list.
+ */
+public class GeneHunt implements Player {
+  
+    public GeneHunt(ScotlandYardView view, String graphFilename) {
+        //TODO: A better AI makes use of `view` and `graphFilename`.
+    }
+
+    @Override
+    public Move notify(int location, Set<Move> moves) {
+        //TODO: Some clever AI here ...
+
+        int choice = new Random().nextInt(moves.size());
+        for (Move move : moves) {
+            if (choice == 0) {
+                return move;
+            }
+            choice--;
+        }
+
+        return null;
+    }
+    
+    /**
+     * Returns the current score of the board.
+     * Factors:
+     *     - Distance from detectives
+     *     - PageRank of current node
+     *     - Overall position (i.e. close to corner)
+     *     - PageRank of detectives nodes
+     *     - Maybe convex hull
+     */
+     private void scoreMrX() {
+        //TODO: Implement the board score heuristic here.
+     }
+     
+     private void scoreDetectives() {
+        //TODO: Implement the board score heuristic here.
+     }
+
+}
---
+++ src/player/GeneHuntFactory.java
@@ -0,0 +1,102 @@
+package player;
+
+import client.application.*;
+import net.PlayerFactory;
+import scotlandyard.Colour;
+import scotlandyard.Player;
+import scotlandyard.ScotlandYardView;
+import scotlandyard.Spectator;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The GeneHuntFactory is a PlayerFactory that
+ * creates a series of Gene Hunt players. By default it assigns
+ * Gene Hunt to be Mr X and all other players are controlled by the
+ * GUI.
+ */
+public class GeneHuntFactory implements PlayerFactory {
+    protected Map<Colour, PlayerType> typeMap;
+
+    public enum PlayerType {AI, GUI}
+
+    String graphFilename;
+
+    protected List<Spectator> spectators;
+    ScotlandYardGame gui;
+    ScotlandYardApplication application;
+    ThreadCommunicator threadCom;
+
+    public GeneHuntFactory(ScotlandYardApplication application, ThreadCommunicator threadCom) {
+        this.application = application;
+        this.threadCom = threadCom;
+        
+        typeMap = new HashMap<Colour, PlayerType>();
+        typeMap.put(Colour.Black, GeneHuntFactory.PlayerType.AI);
+        typeMap.put(Colour.Blue, GeneHuntFactory.PlayerType.GUI);
+        typeMap.put(Colour.Green, GeneHuntFactory.PlayerType.GUI);
+        typeMap.put(Colour.Red, GeneHuntFactory.PlayerType.GUI);
+        typeMap.put(Colour.White, GeneHuntFactory.PlayerType.GUI);
+        typeMap.put(Colour.Yellow, GeneHuntFactory.PlayerType.GUI);
+
+        graphFilename = "resources/graph.txt";
+
+        spectators = new ArrayList<Spectator>();
+    }
+
+    public GeneHuntFactory(ScotlandYardApplication application, ThreadCommunicator threadCom, Map<Colour, PlayerType> typeMap, String graphFilename) {
+        this.application = application;
+        this.threadCom = threadCom;
+        this.typeMap = typeMap;
+        this.graphFilename = graphFilename;
+        spectators = new ArrayList<Spectator>();
+    }
+
+    @Override
+    public Player player(Colour colour, ScotlandYardView view, String graphFilename) {
+        switch (typeMap.get(colour)) {
+            case AI:
+                return new GeneHunt(view, graphFilename);
+            case GUI:
+                return gui(view);
+            default:
+                return new GeneHunt(view, graphFilename);
+        }
+    }
+
+    @Override
+    public void ready() {
+        if (gui != null && application != null) {
+            application.beginGame();
+            application.newAIGame(gui);
+        }
+    }
+
+    @Override
+    public List<Spectator> getSpectators(ScotlandYardView view) {
+        List<Spectator> specs = new ArrayList<Spectator>();
+        specs.add(gui(view));
+        return specs;
+    }
+
+    @Override
+    public void finish() {
+        if (gui != null && application != null) application.endGame();
+    }
+
+
+    private ScotlandYardGame gui(ScotlandYardView view) {
+        if (gui == null) try {
+            gui = new ScotlandYardGame(view, graphFilename, threadCom);
+            spectators.add(gui);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return gui;
+    }
+}
